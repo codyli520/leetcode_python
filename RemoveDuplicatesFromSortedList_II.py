ef5a7176fdb631bee5fa9d2fe7c4068027a963bb
codyli520 <codyli520@gmail.com>
2020-06-21 14:00:23 -0700

    solution with visualization

--- RemoveDuplicatesFromSortedList_II.py
+++ RemoveDuplicatesFromSortedList_II.py
@@ -5,20 +5,49 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
-        dummy = ListNode()
+        dummy = pre = ListNode()
         dummy.next = head
-        pre = dummy
-
-        while head and head.next:
-            if head.val == head.next.val:
-                while head.next and head.val == head.next.val:
-                    head = head.next
-                head = head.next
-                pre.next = head
+        
+        while pre.next:
+            cur = pre.next
+            while cur.next and cur.val == cur.next.val:
+                cur = cur.next
+            
+            if cur != pre.next:
+                pre.next = cur.next
             else:
-                pre = pre.next
-                head = head.next
+                pre = cur
         
         return dummy.next
-        
-        
+
+
+"""
+Visualization of steps
+
+dummy -> 1->1->1->2->3
+ pre
+        cur
+-----------------------
+dummy -> 1->1->1->2->3
+ pre
+              cur
+Removing all duplicates by pre.next = cur.next  
+-----------------------
+dummy ->2->3
+pre
+       cur
+-----------------------
+dummy ->2->3
+       pre
+       cur
+-----------------------
+dummy ->2->3
+       pre
+          cur
+-----------------------
+dummy ->2->3
+          pre
+          cur
+DONE !
+return dummy.next
+"""
